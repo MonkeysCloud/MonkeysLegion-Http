c20c612b997e3126741fb3e37e045880ed3c6dc7
Jorge Peraza <jorge@monkeys.cloud>
2025-05-18 17:59:41 -0600

    include route request handler

--- src/RouteRequestHandler.php
+++ src/RouteRequestHandler.php
@@ -3,20 +3,18 @@ declare(strict_types=1);
 
 namespace MonkeysLegion\Http;
 
+use MonkeysLegion\Router\Router;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use MonkeysLegion\Router\Router;
 
 final class RouteRequestHandler implements RequestHandlerInterface
 {
-    public function __construct(
-        private readonly Router $router
-    ) {}
+    public function __construct(private Router $router) {}
 
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        // delegate straight through to your Router
+        // Router::dispatch() must return a ResponseInterface
         return $this->router->dispatch($request);
     }
 }
